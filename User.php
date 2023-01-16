1069b5955ecc009d137b4cb9658e5c7b5929cac8
Theomrh <92977368+Theomrh@users.noreply.github.com>
2023-01-16 10:03:04 +0100

    init projet local

---
+++ User.php
@@ -0,0 +1,21 @@
+<?php 
+
+    class User {
+        public $id;
+        public $nom;
+        public $prenom;
+        public $username;
+        public $email;
+        public $mdp;
+        public $etablissement;
+    }
+?>
+
+<?php 
+
+    class Jeux {
+        public $id;
+        public $dispo;
+        public $etat;
+    }
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliothèque</title>
+
+    <!-- Import CSS -->
+    <link rel="stylesheet" href="styles/style.css">
+
+    <!-- Import Icons -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</head>
+
+<body>
+    <nav>
+        <div class="navigation">
+            <div class="logo">ALEAS</div>
+            <ul>
+                <li><a href="#" class="nav-select">Accueil</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#" class="btn-link">Ajouter un jeu</a></li>
+                <li><a href="#"><i class="uil uil-user"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
