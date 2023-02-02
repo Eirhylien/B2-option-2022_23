b26d86db4a14de0c543957dd0269f29ba51e3a1f
Theomrh <92977368+Theomrh@users.noreply.github.com>
2023-02-02 08:46:46 +0100

    grid

--- php/pages/index_test.php
+++ php/pages/index_test.php
@@ -18,19 +18,22 @@
 
     <h1> TEST CONNEXION DB</h1>
     <?php
-     try {
-        $db = new PDO('mysql:host=db5011603677.hosting-data.io:3306;dbname=dbs9782335;', 'dbu913389', 'NsU2iLPyJ5kRM4h');
-        //select user
-        $valeurs=false;
-        $converted_res= $valeurs ? 'true' : 'false';
-        echo($converted_res);
-        $user = new User(1,"user","prenom","username","email","mdp",$converted_res);
+        try {
+            $db = new PDO('mysql:host=db5011603677.hosting-data.io:3306;dbname=dbs9782335;', 'dbu913389', 'NsU2iLPyJ5kRM4h');
+            //select user
+            $valeurs = false;
+            $converted_res = $valeurs ? 'true' : 'false';
+            echo ($converted_res);
+            $user = new User(1, "user", "prenom", "username", "email", "mdp", $converted_res);
 
-        $sqlQuery = "INSERT INTO user (email,nom,prenom,username,mdp,etablissement) VALUES ('". $user->email ."','". $user->nom."','". $user->prenom."','". $user->username."','". $user->mdp."',". $user->etablissement.")";
-        echo ($sqlQuery);
-        $usersStatement = $db->prepare($sqlQuery);
-        $usersStatement->execute();
-
+            $sqlQuery = "INSERT INTO user (email,nom,prenom,username,mdp,etablissement) VALUES ('" . $user->email . "','" . $user->nom . "','" . $user->prenom . "','" . $user->username . "','" . $user->mdp . "'," . $user->etablissement . ")";
+            echo ($sqlQuery);
+            $usersStatement = $db->prepare($sqlQuery);
+            $usersStatement->execute();
+        } catch (PDOException $e) {
+            print "Il y a une erreur ici, j'ai rajouté des trucs pour ne plus avoir l'erreur et voir à la correction après";
+        }
+    ?>
 
 
     inscription text
